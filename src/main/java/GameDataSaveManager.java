05e8792484081df1e72bdcf6c5478c4ea88712a1
omegabird113 <omegabird113@outlook.com>
2026-03-16 13:19:17 -0400

    Update dependencies, remove exception handling from GameDataSaveManager

--- src/main/java/GameDataSaveManager.java
+++ src/main/java/GameDataSaveManager.java
@@ -1,7 +1,6 @@
-import com.fasterxml.jackson.databind.ObjectMapper;
+import tools.jackson.databind.ObjectMapper;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.Scanner;
 
 final public class GameDataSaveManager {
@@ -23,15 +22,9 @@ final public class GameDataSaveManager {
                 return generatePlainGameData();
             } else {
                 ObjectMapper objectMapper = new ObjectMapper();
-                try {
-                    SerializableGameData sgd = objectMapper.readValue(file, SerializableGameData.class);
-                    System.out.println("Loaded game at " + gameFileLocation + " successfully. You have a score of " + sgd.score + " to start.");
-                    return new GameData(sgd, new Scorer(), new Scanner(System.in));
-                } catch (IOException e) {
-                    System.out.printf("ERROR: The file failed to load into the Game Object. \ndue to this, the file (%s) has not been loaded.\n", gameFileLocation);
-                    System.out.println("(exact error: " + e + ")");
-                    return generatePlainGameData();
-                }
+                SerializableGameData sgd = objectMapper.readValue(file, SerializableGameData.class);
+                System.out.println("Loaded game at " + gameFileLocation + " successfully. You have a score of " + sgd.score + " to start.");
+                return new GameData(sgd, new Scorer(), new Scanner(System.in));
             }
         } else {
             return generatePlainGameData();
@@ -42,12 +35,7 @@ final public class GameDataSaveManager {
         SerializableGameData sgd = new SerializableGameData(gd);
         ObjectMapper objectMapper = new ObjectMapper();
         File file = new File(filename + ".json");
-        try {
-            objectMapper.writeValue(file, sgd);
-            return true;
-        } catch (IOException e) {
-            System.out.printf("ERROR: Failed to save the game.\nIt was not saved properly to " + filename + ".json.\n(exact error: " + e + ")\n");
-            return false;
-        }
+        objectMapper.writeValue(file, sgd);
+        return true;
     }
 }
