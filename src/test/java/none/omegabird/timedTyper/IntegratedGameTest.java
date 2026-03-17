9c0fffc0e189ea6df8b75a203a1f9aad007ac785
omegabird113 <omegabird113@outlook.com>
2026-03-17 17:36:19 -0400

    Add Tests

---
+++ src/test/java/none/omegabird/timedTyper/IntegratedGameTest.java
@@ -0,0 +1,67 @@
+package none.omegabird.timedTyper;
+
+import org.apache.commons.text.RandomStringGenerator;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+
+public class IntegratedGameTest {
+    private static final Random random = new Random();
+    private static final GameData gameData = new GameData(0,1f,1, new Scorer());
+    private static final Scorer scorer = new Scorer();
+    private static final int totalStringSelectionWeight = 100;
+    private static final int randomStringSelectionWeight = 10;
+
+    @Test
+    void IntegrationTest() {
+        int difficulty = gameData.getDifficulty();
+
+        //each round
+        for (int i = 0; i < 1000; i++) {
+            RoundData roundData = RoundGen.genRoundData(gameData);
+
+            String prompt = roundData.getPrompt();
+            if (prompt.isEmpty()) {
+                Assertions.fail("Prompt is empty.");
+            }
+            int promptLength = prompt.length();
+            RandomStringGenerator randomStringGenerator = new RandomStringGenerator.Builder().withinRange('a', 'z').get();
+            String randomString = randomStringGenerator.generate(promptLength);
+
+            // string maker loop
+            StringBuilder promptBuilder = new StringBuilder(promptLength);
+            for(int j = 0; j < promptLength; j++) {
+                char c1 = prompt.charAt(j);
+                char c2 = randomString.charAt(j);
+                int selector = random.nextInt(1, totalStringSelectionWeight);
+                if (selector <= randomStringSelectionWeight) {
+                    promptBuilder.append(c2);
+                } else {
+                    promptBuilder.append(c1);
+                }
+            }
+            roundData.setUserInput(promptBuilder.toString());
+
+            float timeTaken = random.nextFloat(1f, roundData.getTimeLimit() + random.nextFloat(0f, 15f));
+            roundData.setTimeTaken(timeTaken);
+
+            long score = scorer.calcRoundScore(roundData);
+            if (score < 0) {
+                Assertions.fail("score is negative");
+            } else if (score == 0) {
+                if (roundData.getTimeTaken() < roundData.getTimeLimit() && (roundData.getTimeTaken() / roundData.getTimeLimit()) > 0.125) {
+                    Assertions.fail("Score is improperly 0.");
+                }
+            }
+            gameData.setScore(gameData.getScore() + score);
+            if (gameData.getDifficulty() != difficulty) {
+                difficulty = gameData.getDifficulty();
+                System.out.println("As of round " + i + " difficulty is " + difficulty + ".");
+            }
+        }
+        if (gameData.getDifficulty() < 4) {
+            Assertions.fail("Difficulty too low after 1000 rounds.");
+        }
+    }
+}
---
+++ src/test/java/none/omegabird/timedTyper/ScorerTests.java
@@ -0,0 +1,112 @@
+package none.omegabird.timedTyper;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+
+public class ScorerTests {
+    private static final Random random = new Random();
+    private static final Scorer scorer = new Scorer();
+    private static final GameData gameData = new GameData(0,10f,1, scorer);
+
+    @Test
+    public void noAccuracyTest(){
+        RoundData roundData = new RoundData("the quick brown fox jumps over the lazy dog", 1f, gameData);
+
+        roundData.setTimeTaken(5f);
+        roundData.setUserInput("");
+
+        long score = scorer.calcRoundScore(roundData);
+        Assertions.assertEquals(0, score);
+    }
+
+    @Test
+    public void singleTimeLimitExceededTest(){
+        RoundData roundData = new RoundData("the quick brown fox jumps over the lazy dog", 10f, gameData);
+
+        roundData.setTimeTaken(11f);
+        roundData.setUserInput("the quick brown fox jumps over the lazy dog");
+
+        long score = scorer.calcRoundScore(roundData);
+        Assertions.assertEquals(0, score);
+    }
+
+    @Test
+    public void singleTimeMinimumCheatPreventionTest(){
+        RoundData roundData = new RoundData("the quick brown fox jumps over the lazy dog", 10f, gameData);
+
+        roundData.setTimeTaken(0.112f);
+        roundData.setUserInput("the quick brown fox jumps over the lazy dog");
+
+        long score = scorer.calcRoundScore(roundData);
+        Assertions.assertEquals(0, score);
+    }
+
+    @Test
+    public void idealScoreTest1(){
+        // Normal Multiplier, Fairly High Accuracy, High Time Taken
+
+        RoundData roundData = new RoundData("the quick brown fox jumps over the lazy dog", 10f, gameData);
+
+        roundData.setTimeTaken(7f);
+        roundData.setUserInput("the quik brown fox jump over the lazy dog ");
+
+        long score = scorer.calcRoundScore(roundData);
+        Assertions.assertTrue(score > 30);
+    }
+
+    @Test
+    public void idealScoreTest2(){
+        // Lower Multiplier, Lowest Tested Accuracy, Very High Time Taken
+
+        RoundData roundData = new RoundData("the quick brown fox jumps over the lazy dog", 9f, gameData);
+
+        roundData.setTimeTaken(8.6f);
+        roundData.setUserInput("th quck bown fx jumped over da lazy doh ");
+
+        long score = scorer.calcRoundScore(roundData);
+        Assertions.assertTrue(score > 10);
+    }
+
+    @Test
+    public void idealScoreTest3(){
+        // High Multiplier, High Accuracy, Low Time Taken
+
+        Scorer scorer = new Scorer();
+        GameData gameData = new GameData(0,3f,1, scorer);
+        RoundData roundData = new RoundData("the quick brown fox jumps over the lazy dog", 9f, gameData);
+
+        roundData.setTimeTaken(4f);
+        roundData.setUserInput("the quick brown fox jumps over the lazy dog");
+
+        long score = scorer.calcRoundScore(roundData);
+        Assertions.assertTrue(score > 150);
+    }
+
+    @Test
+    public void massValidScoreBasedOnTimeTest(){
+        final String[] sentenceChoices = new String[5];
+        sentenceChoices[0] = "the quick brown fox jumps over the lazy dog";
+        sentenceChoices[1] = "the quik brown fox jumped over the lazy dog";
+        sentenceChoices[2] = "the quickk brown fox jumps over the lazy dog";
+        sentenceChoices[3] = "th quck broen foxs jumped over le laxy dogge";
+        sentenceChoices[4] = "the quick brown fox jumps, over he lazy dog   ";
+
+        for(int i = 0; i < 50000; i++) {
+            RoundData roundData = new RoundData("the quick brown fox jumps over the lazy dog", 10f, gameData);
+
+            roundData.setTimeTaken(random.nextFloat(1f, 12f));
+            roundData.setUserInput(sentenceChoices[random.nextInt(0, sentenceChoices.length)]);
+
+            long score = scorer.calcRoundScore(roundData);
+            if (score < 0) {
+                Assertions.fail("Score is negative");
+            } else if (score == 0) {
+                if (roundData.getTimeTaken() < roundData.getTimeLimit() && (roundData.getTimeTaken() / roundData.getTimeLimit()) > 0.125) {
+                    Assertions.fail("Score is improperly 0.");
+                }
+            }
+        }
+    }
+}
