2c46ee38f2009a4a42e557ba07163e7663f1a5f8
omegabird113 <omegabird113@outlook.com>
2026-03-18 16:00:50 -0400

    Move terminal logic to Console.java

---
+++ src/main/java/none/omegabird/timedTyper/Console.java
@@ -0,0 +1,60 @@
+package none.omegabird.timedTyper;
+
+import java.util.Scanner;
+
+import static none.omegabird.timedTyper.Main.roundAutoSaveLocation;
+
+public class Console {
+    private static GameData askForLastGameLoad(Scanner scanner) {
+        GameData gd;
+        System.out.print("Would you like to load your previous game from the auto-save location? ");
+        String loadInput = scanner.nextLine();
+        if (loadInput.equalsIgnoreCase("yes")) {
+            gd = GameDataSaveManager.tryLoad(roundAutoSaveLocation);
+        } else {
+            gd = new GameData(0, 1f, 1);
+        }
+        return gd;
+    }
+
+    public static GameData load(String[] args, Scanner scanner) {
+        GameData gd;
+        try {
+            if (!args[0].isEmpty()) {
+                gd = GameDataSaveManager.tryLoad(args[0]);
+            } else {
+                gd = askForLastGameLoad(scanner);
+            }
+        } catch (IndexOutOfBoundsException e) {
+            gd = askForLastGameLoad(scanner);
+        }
+        return gd;
+    }
+
+    public static void play(GameData gd, Scanner scanner) {
+        final InputHandler ih = new InputHandler(scanner, new Scorer());
+
+        System.out.println("Press enter to play: ");
+        scanner.nextLine();
+
+        while(true) {
+            gd = ih.callRound(RoundGen.genRoundData(gd));
+            GameDataSaveManager.trySave(roundAutoSaveLocation, gd);
+            System.out.print("\nEnter 'stop' to stop, to end the game or anything else to continue: ");
+            String input = scanner.nextLine().trim();
+            if (input.equalsIgnoreCase("stop")) {
+                System.out.print("Would you like to save this game? (Note: no mater what, the latest round is always auto-saved to .lastGame.json)\nEnter 'no' to not save somewhere else: ");
+                String saveInput = scanner.nextLine();
+                if (!saveInput.equalsIgnoreCase("no")) {
+                    boolean isNameValid = false;
+                    while(!isNameValid) {
+                        System.out.print("Enter the location & name of your save? (don't include the file extension)): ");
+                        String fileNameInput = scanner.nextLine().trim();
+                        isNameValid = GameDataSaveManager.trySave(fileNameInput, gd);
+                    }
+                }
+                break;
+            }
+        }
+    }
+}
--- src/main/java/none/omegabird/timedTyper/Main.java
+++ src/main/java/none/omegabird/timedTyper/Main.java
@@ -3,51 +3,12 @@ package none.omegabird.timedTyper;
 import java.util.Scanner;
 
 public class Main {
-    private static final String roundAutoSaveLocation = GameDataSaveManager.getGameDataPath() + "/" + ".lastGame";
+    protected static final String roundAutoSaveLocation = GameDataSaveManager.getGameDataPath() + "/" + ".lastGame";
 
     static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-        final InputHandler ih = new InputHandler(scanner, new Scorer());
-
-        GameData gd;
-        if (!args[0].isEmpty()) {
-            gd = GameDataSaveManager.tryLoad(args[0]);
-        } else {
-            System.out.print("Would you like to load your previous game from the auto-save location? ");
-            String loadInput = scanner.nextLine();
-            if (loadInput.equalsIgnoreCase("yes")) {
-                try {
-                    gd = GameDataSaveManager.tryLoad(roundAutoSaveLocation);
-                } catch (IndexOutOfBoundsException e) {
-                    gd = new GameData(0, 1f, 1);
-                }
-            } else {
-                gd = new GameData(0, 1f, 1);
-            }
-        }
-
-        System.out.println("Press enter to play: ");
-        scanner.nextLine();
-
-        while(true) {
-            gd = ih.callRound(RoundGen.genRoundData(gd));
-            GameDataSaveManager.trySave(roundAutoSaveLocation, gd);
-            System.out.print("\nEnter 'stop' to stop, to end the game or anything else to continue: ");
-            String input = scanner.nextLine().trim();
-            if (input.equalsIgnoreCase("stop")) {
-                System.out.print("Would you like to save this game? (Note: no mater what, the latest round is always auto-saved to .lastGame.json)\nEnter 'no' to not save somewhere else: ");
-                String saveInput = scanner.nextLine();
-                if (!saveInput.equalsIgnoreCase("no")) {
-                    boolean isNameValid = false;
-                    while(!isNameValid) {
-                        System.out.print("Enter the location & name of your save? (don't include the file extension)): ");
-                        String fileNameInput = scanner.nextLine().trim();
-                        isNameValid = GameDataSaveManager.trySave(fileNameInput, gd);
-                    }
-                }
-                scanner.close();
-                break;
-            }
-        }
+        final GameData gd = Console.load(args, scanner);
+        Console.play(gd, scanner);
+        scanner.close();
     }
 }
